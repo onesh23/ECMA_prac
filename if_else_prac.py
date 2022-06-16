56618ae98df578d3777a869e7a9e11480aa7303d
Onesh <onesh1997@gmail.com>
2022-06-16 14:34:18 +0530

    initial : commit

---
+++ if_else_prac.py
@@ -0,0 +1,59 @@
+# `1 checking is rectangle or square 
+
+# length = int(input("Enter the Length : "))
+# breadth = int(input("Enter the Breadth : "))
+# if length == breadth:
+#     print("This is Square")
+# else:
+#     print("This is rectangle")
+
+# 2 greatest among two value
+
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+
+# if num1 > num2:
+#     print(num1, " is greater than ", num2)
+
+# elif num2 > num1:
+#     print(num2, " is greater than ", num1)
+# else:
+#     print(num1 ," and ",num2, " both are equal")
+
+
+# 3 shop gives 10% discount on above 1000
+
+# unit = 100
+# quantity = int(input("Enter Amount of Quantity Required : "))
+
+# total = unit  * quantity
+
+# if total > 1000:
+#     print("you have got 10'%' discount :")
+#     discount = (total * 10 )/100
+#     # print(discount)
+#     final_total = total - discount
+#     print("You have to pay total amount : ", total, " with discount  10% ",discount, " you have to pay ", final_total)
+# else:
+#     print("Your total amount is ", total)
+
+# 4 reversing digit
+
+# number_1 = input("Enter a digit number : ")
+# print(number_1[::-1])
+
+# 5 work 
+# age = int(input("Enter your Age : "))
+# gender = input("Enter your Gender (M or F) : ")
+# marrried = input("Enter Marital status (Y or N)  : ")
+
+# if gender == 'F':
+#     print("She Will Work in Urban Areas only")
+# elif gender == 'M' and age >= 20 or age <= 40:
+#     print("You can work Anywhere")
+# elif gender == 'M' and age > 40 or age <= 60:
+#     print("You have to work in urban areas only")
+# else:
+#     print("Error")
+
+
